d471718c6ad42d0c58e962dff4e624727a60d30a
FelipeG123 <felipegandolpho95@gmail.com>
2024-10-18 12:51:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function gerarNumero() {
     const numero = Math.floor(Math.random() * 100) + 1; // Gera número entre 1 e 100
-    alert("Número gerado: " + numero);
+    document.getElementById('resultado').innerText = "Número gerado: " + numero;
 }
